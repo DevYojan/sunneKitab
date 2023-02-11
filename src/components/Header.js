17e2ef13fd16c19fb866ff9a127aafb6f1ff8ca7
Yojan Regmi <devyojan@gmail.com>
2023-02-11 20:49:35 +0545

    typo fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({ books }) => {
     <div className='header'>
       <Link to='/' className='logoTitle'>
         <div className='logo'></div>
-        <h3 className='title'>Sunne Kitaab</h3>
+        <h3 className='title'>Sunne Kitab</h3>
       </Link>
       <Link to='/search' state={{ topSearchedBooks: books.slice(0, 4), books: books }}>
         <i className='fa-solid fa-magnifying-glass fa-2x'></i>
